66effe60d7eb3236d2c4976a5dc8f68e6eb761b8
Rajmund09 <prabhushankarmund@gmail.com>
2025-12-28 01:26:52 +0530

    Update clac.js

--- clac.js
+++ clac.js
@@ -1,29 +1,29 @@
 function calculate() {
-      let rendom = Math.random();
-      console.log(rendom);
+  let rendom = Math.random();
+  console.log(rendom);
 
-      let a = document.getElementById("Enter First number").value;
-      let c = document.getElementById("Enter operation").value;
-      let b = document.getElementById("Enter second number").value;
+  let a = document.getElementById("Enter First number").value;
+  let c = document.getElementById("Enter operation").value;
+  let b = document.getElementById("Enter second number").value;
 
-      let obj = {
-        "+": "-",
-        "*": "+",
-        "-": "/",
-        "/": "**"
-      };
+  let obj = {
+    "+": "-",
+    "*": "+",
+    "-": "/",
+    "/": "**"
+  };
 
-      let outputText;
-      let expression;
+  let outputText;
+  let expression;
 
-      if (rendom > 0.1) {  
-        expression = `${a} ${c} ${b}`;
-        outputText = `hear is your correct answer cutiii = ${eval(expression)}`;
-      } else {
-        c = obj[c];
-        expression = `${a} ${c} ${b}`;
-        outputText = `chall nikal galt he = ${eval(expression)}`;
-      }
+  if (rendom > 0.1) {
+    expression = `${a} ${c} ${b}`;
+    outputText = `hear is your correct answer cutiii = ${eval(expression)}`;
+  } else {
+    c = obj[c];
+    expression = `${a} ${c} ${b}`;
+    outputText = `chall nikal galt he = ${eval(expression)}`;
+  }
 
-      document.getElementById("ans").innerText = outputText;
-    }
+  document.getElementById("ans").innerText = outputText;
+}
